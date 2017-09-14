c49fd0691283e3dbdd352c50ddb71e0699b0ec8b
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-14 09:42:56 -0400

    13/09/2017-A1Q1 completed with comments

--- src/A1Q1.java
+++ src/A1Q1.java
@@ -8,14 +8,20 @@
  * @author simon7323
  */
 public class A1Q1 {
+    //method to find the digital sum of all the digits within an integer
 
     public int digitalSum(int n) {
+        //condition when n is less than 10
+        //return integer
         if (n < 10) {
             return n;
-        }else{
-            int right = n%10;
-            int left = n/10;
-            return digitalSum(left)+digitalSum(right);
+            //condition when greater than 10
+        } else {
+            //take the remainder and the quotient 
+            int right = n % 10;
+            int left = n / 10;
+            //add both remainder and quotient together
+            return digitalSum(left) + digitalSum(right);
         }
     }
 
@@ -23,7 +29,13 @@ public class A1Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create variable to test the boundaries
         A1Q1 test = new A1Q1();
+        int digSum = test.digitalSum(126);
+        System.out.println(digSum);
+        int digSum2 = test.digitalSum(49);
+        System.out.println(digSum2);
+        int digSum3 = test.digitalSum(12);
+        System.out.println(digSum3);
     }
 }

e4f0c210c14d099e14dec6aa6c392f05dfbcd836
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-15 09:39:11 -0400

    15/09/2017-A2Q2 completed with comments

--- src/A2Q2.java
+++ src/A2Q2.java
@@ -17,29 +17,34 @@ public class A2Q2 {
             return n;
             //condition when greater than 10
         } else {
-            //take the remainder and the quotient 
+            //yields the rightmost digit
             int right = n % 10;
+            //removes the rightmost digit
             int left = n / 10;
-            //add both remainder and quotient together
+            //add both left and right digits
             return digitalSum(left) + digitalSum(right);
         }
     }
+    //method to find the root of number and add all digits together
 
     public int digitalRoot(int n) {
+        //condition to 
         if (n < 10) {
             return n;
-        }else{
-        n = digitalSum(n);
-        
-    }return digitalRoot(digitalSum(n));
+        } else {
+            n = digitalSum(n);
+
+        }
+        return digitalRoot(digitalSum(n));
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //create variable to test boundaries
         A2Q2 test = new A2Q2();
-        int digRoot=test.digitalRoot(2019);
+        int digRoot = test.digitalRoot(2019);
         System.out.println(digRoot);
         int digRoot2 = test.digitalRoot(126);
         System.out.println(digRoot2);
@@ -47,6 +52,6 @@ public class A2Q2 {
         System.out.println(digRoot3);
         int digRoot4 = test.digitalRoot(276);
         System.out.println(digRoot4);
-                
+
     }
 }

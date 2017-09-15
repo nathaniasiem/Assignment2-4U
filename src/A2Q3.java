401af97c9aa7e60764c3b97218b9772edf116245
simon7323 <simon7323@HRH-IC0027289.wrdsb.ca>
2017-09-15 10:42:59 -0400

    15/09/2017 -A2Q3 completed with comments

--- src/A2Q3.java
+++ src/A2Q3.java
@@ -11,19 +11,22 @@ public class A2Q3 {
 
     public int triangle(int n) {
         //base case
-        if(n<=0){
-            return n;
-            
-        }else{
-            return triangle(n-1)+triangle();
-        }
+        //condition when nth number equals to 0, output equals 0
+        if(n==0){
+            return n=0;
+        //condition when nth number equals to 1,output equals 1
+        }else if (n==1){
+            return n=1;
+    }
+        //output the nth number plus the previous output number
+        return n+ triangle (n-1);
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create variable to test boundaries
         A2Q3 test = new A2Q3();
         int triangle0 = test.triangle(0);
         System.out.println(triangle0);
@@ -33,6 +36,8 @@ public class A2Q3 {
         System.out.println(triangle2);
         int triangle3 = test.triangle(3);
         System.out.println(triangle3);
+        int triangle4 = test.triangle(4);
+        System.out.println(triangle4);
         
     }
 }

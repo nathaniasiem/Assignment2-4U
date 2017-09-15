5bc4db7bb32d9437a2909519387601409320666d
simon7323 <simon7323@HRH-IC0027289.wrdsb.ca>
2017-09-15 11:10:18 -0400

    15/09/2017 -A2Q4  initial commit

---
+++ src/A2Q4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author simon7323
+ */
+public class A2Q4 {
+
+    public int hailstone(int n){
+        return 0;
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

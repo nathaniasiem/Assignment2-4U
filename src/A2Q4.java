bf3f5b2eaab2ce9f30a15ea4aab4d4c59ce6726c
candy <candy@192.168.0.11>
2017-09-16 12:34:39 -0400

    16/09/2017-A2Q4completed but not giving out correct output

--- src/A2Q4.java
+++ src/A2Q4.java
@@ -9,14 +9,29 @@
  */
 public class A2Q4 {
 
-    public int hailstone(int n){
-        return 0;
-        
+    public int hailstone(int n) {
+        //create base case
+        if (n == 1) {
+            return n;
+        }
+        if (n % 2 == 1) {
+            n=(3 * n + 1);
+            return hailstone(n);
+        } else {
+            n=n/2;   
+        }return hailstone(n);
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create variable to test boundaries
+        A2Q4 test = new A2Q4();
+        int hStone1 = test.hailstone(5);
+        System.out.println(hStone1);
+        int hStone2 = test.hailstone(12);
+        System.out.println(hStone2);
+
     }
 }

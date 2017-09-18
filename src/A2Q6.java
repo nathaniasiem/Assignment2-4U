9436e2838a38fca65c9d9b62b20524cf075ae700
simon7323 <simon7323@HRH-IC0027294.wrdsb.ca>
2017-09-18 13:02:40 -0400

    18/09/2017 - A2Q6 initial commit

---
+++ src/A2Q6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author simon7323
+ */
+public class A2Q6 {
+public void convert(int n, int b){
+    //create base case
+    String code="";
+    
+    if(n)
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

9e9dfe7efa64345f1d2a3bff5013106a6721352a
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-19 10:13:56 -0400

    19/09/2017-A2Q6

--- src/A2Q6.java
+++ src/A2Q6.java
@@ -8,13 +8,26 @@
  * @author simon7323
  */
 public class A2Q6 {
-public void convert(int n, int b){
-    //create base case
-    String code="";
-    
-    if(n)
-}
+
+    public String convert(int n, int b) {
+        
+        //condition when nth number is 0
+        if (n == 0) {
+            //output nothing
+            return "";
+            //condition when nth number is not 0
+        } else {
+            //convert the number by dividing the nth number with base and adding the remainder
+            return convert(n / b, b) + (n % b);   
+        }
+
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        A2Q6 test = new A2Q6();
+        String convertTest=test.convert(1000, 16);
+        System.out.println(convertTest);
+        
     }
 }
+

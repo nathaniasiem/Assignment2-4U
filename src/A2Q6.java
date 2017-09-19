09c3851876bad6e1e3454f703ecfc378b310d3a2
simon7323 <simon7323@HRH-IC0027316.wrdsb.ca>
2017-09-19 10:50:54 -0400

    19/09/2017-A2Q6 added conditions in trying to replace double remainder into letters

--- src/A2Q6.java
+++ src/A2Q6.java
@@ -10,24 +10,44 @@
 public class A2Q6 {
 
     public String convert(int n, int b) {
-        
+        //create base case 
+        //use later to replace remainders greater than/equal to 10 
+        String word = "";
+
+
         //condition when nth number is 0
         if (n == 0) {
             //output nothing
-            return "";
+            return word = "";
             //condition when nth number is not 0
-        } else {
+        } else if (n > 0) {
             //convert the number by dividing the nth number with base and adding the remainder
-            return convert(n / b, b) + (n % b);   
-        }
+            return convert(n / b, b) + (n % b);
+        } else if (n % b == 10) {
+            word = "A";
+        } else if (n % b == 11) {
+            word = "B";
 
+       } else if (n % b == 12) {            
+           word = "C";
+
+        }else if(n % b==13){
+            word="D";
+        }else if(n % b==14){
+            word="E";
+        }else if(n % b==15){
+            word="F";
+        }
+        return convert(n / b, b) + (n % b) + word;
     }
+
+    
+
     public static void main(String[] args) {
         // TODO code application logic here
         A2Q6 test = new A2Q6();
-        String convertTest=test.convert(1000, 16);
+        String convertTest = test.convert(1000, 16);
         System.out.println(convertTest);
-        
+
     }
 }
-

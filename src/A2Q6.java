3e33bc4c8b1c79f93b904ebf8eb15a0d407b7320
simon7323 <simon7323@HRH-IC0027292.wrdsb.ca>
2017-09-19 14:19:34 -0400

    19/09/2017 A2Q6 completed with comments

--- src/A2Q6.java
+++ src/A2Q6.java
@@ -10,35 +10,22 @@
 public class A2Q6 {
 
     public String convert(int n, int b) {
-        //create base case 
-        //use later to replace remainders greater than/equal to 10 
-        String word = "";
-
-
+        //create base case
+        //use later as temporary storage to replace remainders greater than/equal to 10 
+        String code = "";
+        
         //condition when nth number is 0
         if (n == 0) {
             //output nothing
-            return word = "";
-            //condition when nth number is not 0
-        } else if (n > 0) {
-            //convert the number by dividing the nth number with base and adding the remainder
-            return convert(n / b, b) + (n % b);
-        } else if (n % b == 10) {
-            word = "A";
-        } else if (n % b == 11) {
-            word = "B";
-
-       } else if (n % b == 12) {            
-           word = "C";
-
-        }else if(n % b==13){
-            word="D";
-        }else if(n % b==14){
-            word="E";
-        }else if(n % b==15){
-            word="F";
-        }
-        return convert(n / b, b) + (n % b) + word;
+            return code = "";
+        } 
+        //create array to hold all numbers and letters
+        String [] letter = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+       //replace base case with remainder using letters and numbers from array
+        code=letter[n%b];
+        //recall method (includes with replacement letters if conditions are met)
+        //otherwise positive integer is divided by base
+        return convert(n / b, b) + code;
     }
 
     
@@ -46,8 +33,14 @@ public class A2Q6 {
     public static void main(String[] args) {
         // TODO code application logic here
         A2Q6 test = new A2Q6();
-        String convertTest = test.convert(1000, 16);
-        System.out.println(convertTest);
+        String convertTest1 = test.convert(1000, 8);
+        System.out.println(convertTest1);
+        System.out.println("-----------------");
+        String convertTest2 = test.convert(1000, 16);
+        System.out.println(convertTest2);
+        System.out.println("-----------------");
+        String convertTest3 = test.convert(1000,2);
+        System.out.println(convertTest3);
 
     }
 }

da13e623025adaf4588191c7748000ed2fe13e69
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-21 10:02:15 -0400

    21/09/2017 A2Q7 completed with comments

--- src/A2Q7.java
+++ src/A2Q7.java
@@ -9,10 +9,32 @@
  */
 public class A2Q7 {
 
+    public boolean isPalindrome(String s, int length) {
+        //create a base case which will return true if condition matches
+        if (length <=1) {
+            return true;
+        }
+        //set the first letter as a string
+       String frstLetter =s.substring(0,1);
+       //set the last letter as a string
+       String lstLetter = s.substring(length-1);
+       
+       if(frstLetter.equals(lstLetter)){
+           return isPalindrome(s.substring(1,length-1),length-2);
+       }else{
+           return false;
+       }
+       
+           
+        }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create variable to test boundaries
+        A2Q7 test = new A2Q7();
+        System.out.println(test.isPalindrome("racecar", 7));
+        System.out.println(test.isPalindrome("radar",5));
+        System.out.println(test.isPalindrome("Nathania",8));
     }
 }
